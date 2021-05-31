c03ebcd481664cc799aa59c84b3ddd935b801bb3
tyronemc2 <tyrone@middletons.co.za>
2021-05-31 15:50:02 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'jessica@dietitiansdiaries.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'orders@dietitiansdiaries.com'),
+        'name' => env('MAIL_FROM_NAME', 'Dietitians Diaries'),
     ],
 
     /*
