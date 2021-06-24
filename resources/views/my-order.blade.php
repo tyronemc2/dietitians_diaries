c1e947b95ffe61fd8d1f21338ba0cab48e67c171
tyronemc2 <tyrone@middletons.co.za>
2021-06-24 16:08:11 +0200

    Update my-order.blade.php

--- resources/views/my-order.blade.php
+++ resources/views/my-order.blade.php
@@ -58,7 +58,7 @@
                                 <div>{{ $order->id }}</div>
                             </div><div>
                                 <div class="uppercase font-bold">Total</div>
-                                <div>{{ presentPrice($order->billing_total) }}</div>
+                                <div>{{ $order->billing_total }}</div>
                             </div>
                         </div>
                         <div>
@@ -112,7 +112,7 @@
                     <div class="order-products">
                         @foreach ($products as $product)
                             <div class="order-product-item">
-                                <div><img src="{{ asset($product->image) }}" alt="Product Image"></div>
+                                <div><img src="{{ productImage($product->image) }}" alt="Product Image"></div>
                                 <div>
                                     <div>
                                         <a href="{{ route('shop.show', $product->slug) }}">{{ $product->name }}</a>
