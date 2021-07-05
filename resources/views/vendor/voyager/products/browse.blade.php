2f7c1a4a2f77a367dad79f4f029a71f9d582fe7f
tyronemc2 <tyrone@middletons.co.za>
2021-07-05 11:49:44 +0200

    Update browse.blade.php

--- resources/views/vendor/voyager/products/browse.blade.php
+++ resources/views/vendor/voyager/products/browse.blade.php
@@ -171,7 +171,7 @@
                                                 @else
                                                     @include('voyager::multilingual.input-hidden-bread-browse')
                                                     @if ($row->display_name == 'price')
-                                                        <span>R{{ $data->{$row->field} }}</span>
+                                                        <span>R{{ $data->{$row->field} /100 }}</span>
                                                     @else
                                                         <span>{{ $data->{$row->field} }}</span>
                                                     @endif
